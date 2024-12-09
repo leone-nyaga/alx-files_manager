a247d18e4a96e7e19071d952a3a5c025c0873626
leone-nyaga <leonenyaga@gmail.com>
2024-12-09 10:40:14 +0300

    ALX FILE MANAGER

--- utils/redis.js
+++ utils/redis.js
@@ -10,53 +10,55 @@ class RedisClient {
     });
   }
 
-  /* check if redis connection is alive(connected and without error */
+  /* check if redis connection is alive (connected and without error) */
   isAlive() {
     return this.client.status === 'ready';
   }
 
   /* asynchronous function get that takes a string key as argument and returns the Redis value stored for this key */
   async get(key) {
-    return new promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       this.client.get(key, (err, value) => {
         if (err) {
           reject(err);
-	} else {
-	  resolve(value);
-	}
+        } else {
+          resolve(value);
+        }
       });
     });
   }
 
-  /* asynchronous function set that takes a string key,
-   * a value and a duration in second as arguments to store it in Redis
+  /* asynchronous function set that takes a string key, 
+   * a value, and a duration in seconds as arguments to store it in Redis 
    * (with an expiration set by the duration argument)
    */
   async set(key, value, duration) {
-    return new promise((resolve, reject) => {
-      this.client.set(key, value, 'EX', duration, (err) => {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err, result) => {
         if (err) {
           reject(err);
-	} else {
-	  resolve(value);
-	}
+        } else {
+          resolve(result); // Resolving with result (e.g., 'OK')
+        }
       });
     });
   }
 
-  /* asynchronous function del that takes a string key as argument and remove the value in Redis for this key */
+  /* asynchronous function del that takes a string key as argument and removes the value in Redis for this key */
   async del(key) {
-    return new promise((resolve, reject) => {
-      this.client.del(key, (err, value) => {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, result) => {
         if (err) {
-	  reject(err);
-	} else {
-	  resolve(value);
-	}
+          reject(err);
+        } else {
+          resolve(result); // Resolving with the result (number of keys deleted)
+        }
       });
     });
   }
 }
 
+// Create and export the RedisClient instance
 const redisClient = new RedisClient();
 module.exports = redisClient;
+
