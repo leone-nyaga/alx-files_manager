a44383c2117829a4911d8fc68b73bde8573464e9
leone-nyaga <leonenyaga@gmail.com>
2024-12-09 10:28:16 +0300

    ALX FILE MANAGER

--- redis.js
+++ redis.js
@@ -15,4 +15,48 @@ class RedisClient {
     return this.client.status === 'ready';
   }
 
-  
+  /* asynchronous function get that takes a string key as argument and returns the Redis value stored for this key */
+  async get(key) {
+    return new promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+	} else {
+	  resolve(value);
+	}
+      });
+    });
+  }
+
+  /* asynchronous function set that takes a string key,
+   * a value and a duration in second as arguments to store it in Redis
+   * (with an expiration set by the duration argument)
+   */
+  async set(key, value, duration) {
+    return new promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err) => {
+        if (err) {
+          reject(err);
+	} else {
+	  resolve(value);
+	}
+      });
+    });
+  }
+
+  /* asynchronous function del that takes a string key as argument and remove the value in Redis for this key */
+  async del(key) {
+    return new promise((resolve, reject) => {
+      this.client.del(key, (err, value) => {
+        if (err) {
+	  reject(err);
+	} else {
+	  resolve(value);
+	}
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
